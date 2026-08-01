eacdf0ed82ebd67cc8f345ee9df4894f29ddeceb
Yashsolanki27 <solankiyash7227@gmail.com>
2026-08-01 22:49:28 +0200

    storing application data

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("AI Fluency Lab Started");
+const modules = [
+  {
+    title: "AI Fluency Framework",
+    description: "Learn human AI collaboration",
+  },
+
+  {
+    title: "Generative AI",
+    description: "Understand AI capabilities",
+  },
+
+  {
+    title: "Prompt Engineering",
+    description: "Create better instructions",
+  },
+];
+
+console.log(modules);
